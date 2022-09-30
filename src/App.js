be0ab4c46de60d5af479de9aa7d421ba87282e8b
dungthinhtv <102164139+dungthinhtv@users.noreply.github.com>
2022-09-30 10:30:48 +0700

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,38 +1,17 @@
 import React from 'react';
 import './App.css';
 
-import AuthContextProvider from './contexts/AuthContext';
-import PostContextProvider from './contexts/PostContext';
-
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Landing from './components/layout/Landing';
-import ProtectedRoute from './components/routing/ProtectedRoute';
-import Dashboard from './views/Dashboard';
-import About from './views/About';
-import Auth from './views/Auth';
+import Login from './components/layout/Login';
 
 export default function App() {
   return (
-    <AuthContextProvider>
-      <PostContextProvider>
-        <Router>
-          <Switch>
-            <Route exact path="/" component={Landing} />
-            <Route
-              exact
-              path="/login"
-              render={(props) => <Auth {...props} authRoute="login" />}
-            />
-            <Route
-              exact
-              path="/register"
-              render={(props) => <Auth {...props} authRoute="register" />}
-            />
-            <ProtectedRoute exact path="/dashboard" component={Dashboard} />
-            <ProtectedRoute exact path="/about" component={About} />
-          </Switch>
-        </Router>
-      </PostContextProvider>
-    </AuthContextProvider>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/login" component={Login} />
+      </Switch>
+    </Router>
   );
 }
